6f3d94c79158ccae86869d63fd83d0fa6d1e88a1
cryatal <1525978952@qq.com>
2017-12-04 10:07:43 +0800

    1

--- src/pages/offers.vue
+++ src/pages/offers.vue
@@ -1,22 +1,246 @@
 <template>
 	<div class="offers">
-		招贤纳士
+		<div class="main">
+			<div class="top">
+				<span class="text">首页&nbsp;>&nbsp; 招贤纳士</span>
+			</div>
+			<div class="list clearfix">
+				<div class="responsibility">
+					<Collapse active-key="1" accordion>
+						<Panel key="1">
+							<span class="wenzi">资信评级项目经理</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1、参与企业资信评估项目执行工作，完成或协助项目负责人完成项目的实地访谈、调研、客户开发与维护、跟踪、评估、评估报告制作等；</dd>
+								<dd>2、根据部门分工安排，负责多领域多项目的日常跟踪，包括信息搜集整理、行业动态了解、协会及行业展会等联系调研、项目跟踪等；</dd>
+								<dd>3、参与项目的持续督导、后期管理等工作；</dd>
+								<dd>4、负责与合作伙伴的深度沟通、协同完成项目的稳步推进落地；</dd>
+								<dd>5、根据部门工作安排，组织实施部门日常项目工作，协助完成上级领导交办的其他工作。</dd>
+								<dt>任职要求</dt>
+								<dd>1、全日制本科或以上学历，会计、金融、投资、财务管理等相关专业；</dd>
+								<dd>2、有会计师事务所、律师事务所、资信评估公司、银行或投行工作经验优先；</dd>
+								<dd>3、有CPA资格优先；</dd>
+								<dd>4、具备项目评估工作所需的财务、金融、法律等相关专业知识；</dd>
+								<dd>5、具有良好的文字写作能力，具有较强的学习能力、沟通能力、组织协调能力、创新意识、团队合作意识和敬业精神，工作态度勤勉，细心严谨；</dd>
+								<dd>6、具有良好的抗压能力，能适应经常出差的工作要求。</dd>
+							</dl>
+						</Panel>
+						<Panel key="2">
+							<span class="wenzi">资信评估实习生</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1. 数据录入；</dd>
+								<dd>2. 根据相关数据撰写分析报告（包含且不限于财务分析报告，市场分析报告、行业研究报告等）；</dd>
+								<dd>3.领导交代的其他事宜。</dd>
+								<dt>任职要求</dt>
+								<dd>1. 硕士以上学历，新闻学、文学、法学、会计学、财务管理专业应届毕业生；</dd>
+								<dd>2.有较强的逻辑思维能力，具备专业文字功底；</dd>
+								<dd>3. 工作认真、仔细，学习能力强，思维清晰，有良好的沟通协调能力；</dd>
+								<dd>4. 具有良好的职业道德、积极主动配合部门内部事宜。</dd>
+							</dl>
+						</Panel>
+						<Panel key="3">
+							<span class="wenzi">财务分析员</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1. 数据录入；</dd>
+								<dd>2. 财务数据分析；</dd>
+								<dd>3. 财务报告撰写；</dd>
+								<dd>4.领导交代的其他事宜。</dd>
+								<dt>任职要求</dt>
+								<dd>1. 具备一定的财务专业功底， 了解财务报表、财务分析；</dd>
+								<dd>2.有较强的逻辑思维能力，具备专业文字功底；</dd>
+								<dd>3. 工作认真、仔细，学习能力强，思维清晰，有良好的沟通协调能力；</dd>
+								<dd>4. 具有良好的职业道德、积极主动配合部门内部事宜。</dd>
+							</dl>
+						</Panel>
+						<Panel key="4">
+							<span class="wenzi">项目经理</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1、制定新产品立项、产品设计、研发阶段审核等的总体计划，并对所负责项目的进度和项目综合质量负总责；</dd>
+								<dd>2、负责开发周期内的项目进度，风险控制，资源调度，预警及备份应急计划；</dd>
+								<dd>3、跟踪计划实施情况，必要时提出及调整项目计划，对影响项目按时完成的因素要及时发现并找到原因；</dd>
+								<dd>4、监督各部门分支计划的执行，协调本项目执行过程中各部门间的合作和沟通；&nbsp;</dd>
+								<dd>5、对项目的进展情况、阶段性成果，关键里程碑任务的完成情况做定期和及时汇报；</dd>
+								<dd>6、对项目已出现的问题和项目内潜在的问题进行及时沟通，组织相关人员解决处理，提出备选方案，并跟踪执行情况；</dd>
+								<dd>7、对项目进行规范化管理，协同质量管理部做好产品的质量管理；</dd>
+								<dd>8、主持项目结束总结，包括：项目概述，总结项目目标实现情况，项目实施过程回顾、分析，总结项目出现的问题和经验教训。</dd>
+								<dt>任职要求</dt>
+								<dd>1、计算机、通信、电子类专业本科(含)以上学历；</dd>
+								<dd>2、软件公司或互联网公司研发项管理2年以上经验；</dd>
+								<dd>3、从事软件开发至少2年以上,熟悉相关的主流技术。</dd>
+							</dl>
+						</Panel>
+						<Panel key="5">
+							<span class="wenzi">产品经理（后台）</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1、研究，调研、收集、分析的各类需求并形成需求文档；</dd>
+								<dd>2、根据系统规划，实现惠政系统的业务模块；</dd>
+								<dd>3、发起研发流程，与研发团队紧密配合，快速、高效推动研发，并确保产品准时上线；</dd>
+								<dt>任职要求</dt>
+								<dd>1、2年以上系统产品经理经验。</dd>
+								<dd>2、具有较强的业务场景梳理能力及逻辑模型抽象能力，完整清晰地表达自己的设计方案；</dd>
+								<dd>3、接受测试转产品或研发转产品，需要3年以上测试用例设计经验或系统设计经验；</dd>
+								<dd>4、良好的文档写作习惯，能够简单清晰的用相关文档表述出产品的需求和功能；</dd>
+								<dd>5、练使用常用产品设计工具（Axure、Visio、MindManager、PPT等），具有良好的产品设计习惯；</dd>
+								<dd>6、对互联网保持强烈兴趣并具有一定商业灵敏度，能承受较强的工作压力；</dd>
+							</dl>
+						</Panel>
+						<Panel key="6">
+							<span class="wenzi">高级JAVA开发工程师</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1、独立完成小型项目的系统分析、设计，并主导完成详细设计和编码的任务，确保项目的进度和质量；</dd>
+								<dd>2、能够在团队中完成Code Review的任务，确保相关代码的有效性和正确性，并能够通过Code Review提供相关性能以及安全的建议；</dd>
+								<dd>3、独立完成部分较小规模系统的系统分析文档和详细设计文档的撰写；</dd>
+								<dd>4、能够有效地对新人或普通开发工程师进行辅导，帮助其快速成长；</dd>
+								<dd>5、维护和升级现有软件产品和系统，快速定位并修复现有软件缺陷。</dd>
+								<dt>任职要求</dt>
+								<dd>1、本科、硕士及以上学历，计算机等相关专业；</dd>
+								<dd>2、5年以上Java编程经验、web开发经验，理解web标准；</dd>
+								<dd>3、精通web常用框架(SSH,SSM，shiro等),消息队列中间件(rabbit MQ,Active MQ),网络通讯框架(netty,mina),多线程(线程池),分布式(dubbo)等框架,精通jquery等js框架,至少使用过一种富客户端框架,对hadoop有所了解；</dd>
+								<dd>4、熟练使用redis, mogodb等非关系型数据库,mysql,oracle关系型数据库。</dd>
+								<dd>5、有项目管理经验者优先、有大型项目开发经验优先。</dd>
+							</dl>
+						</Panel>
+						<Panel key="7">
+							<span class="wenzi">中级JAVA开发工程师</span>
+							<dl slot="content" class="require">
+								<dt>岗位职责</dt>
+								<dd>1、参与系统的需求调研和需求分析，交付项目需求规格说明书；</dd>
+								<dd>2、参与系统设计和讨论，交付概要设计、详细设计等设计文档；</dd>
+								<dd>3、完成项目关键功能模块的代码实现；</dd>
+								<dd>4、系统开发测试、部署和集成，确保研发软件的安全、质量和性能；</dd>
+								<dd>5、负责解决开发过程中的技术问题，指导初级开发人员共同完成项目目标；</dd>
+								<dt>任职要求</dt>
+								<dd>1、本科及以上学历，计算机等相关专业；</dd>
+								<dd>2、熟练掌握SSH,SSM开发框架,对其工作原理具有深入认识；</dd>
+								<dd>3、熟练掌握 jquery,bootstrap等js,css框架,至少掌握一种富客户端框架；</dd>
+								<dd>4、掌握redis,mogodb非结构化数据库的使用,掌握mysql,oracle数据库的使用,可以对sql进行语句级优化；</dd>
+								<dd>5、具有linux环境下安装,部署软件工作经验；</dd>
+								<dd>6、工作积极主动，服从工作安排，有较强的团队合作精神。</dd>
+							</dl>
+						</Panel>
+					</Collapse>
+				</div>
+				<div class="contact-information">
+					<h4 class="text-danger">联系方式</h4>
+					<hr>
+					<div class="contact-left">
+						<div class="img1"><img :src="contact_1" /></div>
+						<div class="img1"><img :src="contact_2" /></div>
+					</div>
+					<div class="contact-right">
+						<p class="tel">电话：</p>
+						<p class="tel">+86-0553-5801065</p>
+						<p class="mail">邮箱：</p>
+						<p class="">zuoxl@creditstate.cn</p>
+					</div>
+					<!--<dl style="background: #E5E3E0;">
+						<dd>
+							<div><img :src="contact_1" /></div>
+							<p>电话：</p>+86-0553-5801065
+						</dd>
+					</dl>-->
+					<!--<dl style="background: #96938D;">
+						<dd><img :src="contact_2" />
+						<p>邮箱：</p>
+						<p>zuoxl@creditstate.cn</p>
+						</dd>
+					</dl>-->
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {
-	beforeRouteEnter   (to, from, next) {
-		next((vm)=>{
-			vm.$emit('setNav',to.meta.index)
-		})
-	},
-};
+	import contact_1 from './img/hr-1.png';
+	import contact_2 from './img/hr-2.png';
+	export default {
+		data: function() {
+			return {
+
+				contact_1: contact_1,
+				contact_2: contact_2,
+			};
+		},
+		beforeRouteEnter(to, from, next) {
+			next((vm) => {
+				vm.$emit('setNav', to.meta.index)
+			})
+		},
+
+	};
 </script>
 <style lang="less" scoped>
-.offers {
-  width: 1000px;
-  margin: 0 auto;
-}
+	.offers {
+		width: 1000px;
+		margin: 0 auto;
+		.top{
+			position: relative;
+			height: 30px;
+			padding: 4px 15px;
+			margin: 18px 0 28px;
+			border-radius: 5px;
+			color: rgb(255, 255, 255);
+			background: rgb(179, 27, 36);
+			.text {
+				vertical-align: middle;
+				font-size: 12px;
+			}
+		}
+		.responsibility {
+			margin-bottom: 40px;
+			dt {
+				color: #000000;
+				font-size: 14px;
+			}
+			dd {
+				color: #6C6A65;
+				font-size: 14px;
+				line-height: 2em;
+			}
+			.ivu-collapse-content-box {
+				padding-top: 5px !important;
+			}
+			.wenzi {
+				height: 38px;
+				line-height: 38px;
+				font-size: 16px;
+				font-weight: 500;
+				color: #DD4347;
+				cursor: pointer;
+				position: relative;
+			}
+		}
+		.text-danger {
+			color: #DD4347;
+			font-size: 16px;
+			font-weight: 500;
+		}
+		.contact-information {
+			height: 180px;
+			.contact-left {
+				width: 30px;
+				height: 100px;
+				float: left;
+				padding-bottom: 10px;
+				.img1 {
+					padding: 10px 10px 10px 0px;
+				}
+			}
+			.contact-right {
+				float: left;
+				p {
+					font-size: 14px;
+				}
+				.mail {
+					margin-top: 5px;
+				}
+			}
+		}
+	}
 </style>
-
