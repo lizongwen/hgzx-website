ae0f475bfb0776ef0c85afe47314f010a8e70bb8
cryatal <1525978952@qq.com>
2017-12-13 16:18:05 +0800

    11

--- src/pages/financial/iot.vue
+++ src/pages/financial/iot.vue
@@ -1,21 +1,28 @@
 <template>
 	<div class="iot">
-		<p class="title-big">2016年中国国际大数据大会获奖</p>
-		<p class="title-small">惠国征信荣获二零一六年度中国大数据领域创新创业奖</p>
-		<img :src="iot_1" />
-		<p class="title-small">惠国征信荣获二零一六年度中国大数据领域创新人物奖</p>
-		<img :src="iot_2" />
+		<div class="iot-left">
+			<div class="img1"><img :src="iot_1" /></div>
+		</div>
+		<div class="iot-right">
+			<div class="img2"><img :src="iot_2" /></div>
+			<div class="img3"><img :src="iot_3" /></div>
+			<div class="img4"><img :src="iot_4" /></div>
+		</div>
 	</div>
 </template>
 
 <script>
-	import iot_1 from '../img/new-jiang1.png';
-	import iot_2 from '../img/new-jiang2.png';
+	import iot_1 from '../img/iot_1.png';
+	import iot_2 from '../img/iot_2.png';
+	import iot_3 from '../img/iot_3.png';
+	import iot_4 from '../img/iot_4.png';
 	export default {
 		data: function() {
 			return {
 				iot_1: iot_1,
 				iot_2: iot_2,
+				iot_3: iot_3,
+				iot_4: iot_4,
 			};
 		},
 
@@ -24,19 +31,18 @@
 
 <style lang="less" scoped>
 	.iot {
-		text-align: center;
-		.title-big {
-			font-weight: 500;
-			font-size: 18px;
-			color: #CD0000;
-			padding-bottom: 15px;
+		.iot-left {
+			width: 280px;
+			float: left;
+			margin-top: 20px;
 		}
-		.title-small {
-			font-size: 14px;
-			padding-bottom: 13px;
-		}
-		img{
-			padding-bottom: 15px;
+		.iot-right {
+			width: 520px;
+			height: 100%;
+			float: left;
+			img{
+				margin-bottom: 10px;
+			}
 		}
 	}
 </style>
