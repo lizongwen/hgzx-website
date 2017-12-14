6ef61795a5e9768db6c7ad57b92e802e76b345bf
cryatal <1525978952@qq.com>
2017-12-14 14:49:17 +0800

    11

--- src/pages/government/benevolent.vue
+++ src/pages/government/benevolent.vue
@@ -1,32 +1,18 @@
 <template>
-	<div class="credit">
-		<div class="credit-left">
-			<p class="title-big">企业理念 <span>Company Philosophy</span></p>
-			<p class="title-small">以信惠国、以智惠政、以诚惠商、以利惠农、以礼惠民</p>
-			<p class="title-big">企业愿景 <span> Our Vison</span></p>
-			<p class="title-small">打造业内卓越精英团队，树立国内顶级行业标杆，成就国际高端服务品牌</p>
-			<p class="title-big">企业使命<span>Our Mission</span></p>
-			<p class="title-small">运用现代征信技术，构建社会信用体系，促进社会经济发展</p>
-			<p class="title-big">核心价值观 <span> Core Values </span></p>
-			<p class="title-small">尊重、自律、务实、尽职、分享</p>
-			<p class="title-big">发展理念 <span>Organization Development </span></p>
-			<p class="title-small">创建政务信用体系，完善商业信用体系，补充金融信用体系</p>
-		</div>
-		<div class="credit-right">
-			<div class="img1"><img :src="credit_1" /></div>
-			<div class="img2"><img :src="credit_2" /></div>
-		</div>
+	<div class="benevolent">
+		<img :src="benevolent_1" />
+		<div class="img2"><img :src="benevolent_2" /></div>
 	</div>
 </template>
 
 <script>
-	import credit_1 from '../img/logo-right.png';
-	import credit_2 from '../img/img-right.png';
+	import benevolent_1 from '../img/hz_1.png';
+	import benevolent_2 from '../img/hz_2.png';
 	export default {
 		data: function() {
 			return {
-				credit_1: credit_1,
-				credit_2: credit_2,
+				benevolent_1: benevolent_1,
+				benevolent_2: benevolent_2,
 			};
 		},
 
@@ -34,31 +20,9 @@
 </script>
 
 <style lang="less" scoped>
-	.credit {
-		.credit-left {
-			width: 500px;
-			float:left;
-			.title-big {
-				font-weight: 500;
-				font-size: 16px;
-				color: #CD0000;
-				padding-bottom: 10px;
-				span {
-					font-size: 12px;
-					color: #96938D;
-				}
-			}
-			.title-small {
-				font-size: 14px;
-				padding-bottom: 20px;
-			}
-		}
-		.credit-right {
-			width: 300px;
-			float: left;
-		.img1 {
-			padding-bottom: 15px;
-		}
+	.benevolent {
+		.img2{
+			margin-bottom: 15px;
 		}
 	}
 </style>
