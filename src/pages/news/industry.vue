179cbc641294d11c24e5c0246058c21975089c14
cryatal <1525978952@qq.com>
2017-12-21 14:31:44 +0800

    11

--- src/pages/news/industry.vue
+++ src/pages/news/industry.vue
@@ -14,100 +14,173 @@
 </template>
 
 <script>
-import newsDetail1 from "./newsDetail/newsDetail1";
-import newsDetail2 from "./newsDetail/newsDetail2";
-import newsDetail3 from "./newsDetail/newsDetail3";
-// import newsDetail4 from "./newsDetail/newsDetail4";
-// import newsDetail5 from "./newsDetail/newsDetail5";
-// import newsDetail6 from "./newsDetail/newsDetail6";
-// import newsDetail7 from "./newsDetail/newsDetail7";
-// import newsDetail8 from "./newsDetail/newsDetail8";
-// import newsDetail9 from "./newsDetail/newsDetail9";
-// import newsDetail10 from "./newsDetail/newsDetail10";
-export default {
-  name: "company-news",
-  components: {
-    newsDetail1: newsDetail1,
-    newsDetail2: newsDetail2,
-    newsDetail3: newsDetail3
-    // newsDetail4: newsDetail4,
-    // newsDetail5: newsDetail5,
-    // newsDetail6: newsDetail6,
-    // newsDetail7: newsDetail7,
-    // newsDetail8: newsDetail8,
-    // newsDetail9: newsDetail9,
-    // newsDetail10: newsDetail10,
-  },
-  data: function() {
-    return {
-      current: "newsDetail1",
-      showList: true,
-      news: [
-        {
-          typeid: 2,
-          id: "1001",
-          type: "行业新闻",
-          date: "2017-05-05",
-          title: "芜湖新闻频道“丹丹面对面”栏目邀请惠国征信徐博士谈征信",
-          detail:
-            "<p>公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1公司动态1</p>"
-        },
-        {
-          typeid: 1,
-          id: "1002",
-          type: "行业新闻",
-          date: "2017-05-05",
-          title: "惠国征信举办《建模方法论SEMMA建模路径》培训",
-          detail:
-            "公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2公司动态2"
-        },
-      ],
-      newsDetail: {}
-    };
-  },
-  methods: {
-    showDetail: function(current) {
-      console.log(current);
-      this.showList = false;
-      this.newsDetail = this.news[current];
-      this.current = "newsDetail" + (current + 1);
-    },
-    backList() {
-      this.showList = true;
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      if (to.params.id) {
-        vm.showDetail(to.params.id);
-      }
-    });
-  },
-  mounted: function() {
-    this.$emit("setCrumb", "行业新闻");
-  }
-};
+	import newsDetail1 from "./industry/newsDetail1";
+	import newsDetail2 from "./industry/newsDetail2";
+	import newsDetail3 from "./industry/newsDetail3";
+	import newsDetail4 from "./industry/newsDetail4";
+	import newsDetail5 from "./industry/newsDetail5";
+	import newsDetail6 from "./industry/newsDetail6";
+	import newsDetail7 from "./industry/newsDetail7";
+	import newsDetail8 from "./industry/newsDetail8";
+	import newsDetail9 from "./industry/newsDetail9";
+	import newsDetail10 from "./industry/newsDetail10";
+	import newsDetail11 from "./industry/newsDetail11";
+	import newsDetail12 from "./industry/newsDetail12";
+	import newsDetail13 from "./industry/newsDetail13";
+	import newsDetail14 from "./industry/newsDetail14";
+	export default {
+		name: "company-news",
+		components: {
+			newsDetail1: newsDetail1,
+			newsDetail2: newsDetail2,
+			newsDetail3: newsDetail3,
+			newsDetail4: newsDetail4,
+			newsDetail5: newsDetail5,
+			newsDetail6: newsDetail6,
+			newsDetail7: newsDetail7,
+			newsDetail8: newsDetail8,
+			newsDetail9: newsDetail9,
+			newsDetail10: newsDetail10,
+			newsDetail11: newsDetail11,
+			newsDetail12: newsDetail12,
+			newsDetail13: newsDetail13,
+			newsDetail14: newsDetail14,
+		},
+		data: function() {
+			return {
+				current: "newsDetail1",
+				showList: true,
+				news: [{
+						typeid: 1,
+						id: "1001",
+						date: "2017-10-16",
+						title: "1市长谈信用：五大体系推进芜湖诚信建设制度化",
+					},
+					{
+						typeid: 1,
+						id: "1002",
+						date: "2017-07-25",
+						title: "2首届“中国城市信用建设高峰论坛”圆满落幕",
+					},
+					{
+						name: "newsDetail3",
+						typeid: 1,
+						id: "1003",
+						date: "2017-07-12",
+						title: "芜湖市政府发布中小企业信用贷试点工作实施意见",
+					},
+					{
+						typeid: 1,
+						id: "1004",
+						date: "2017-06-29",
+						title: "杭州保姆纵火案反思：如何保障雇主知情权",
+					},
+					{
+						typeid: 1,
+						id: "1005",
+						date: "2017-05-26",
+						title: "国家发改委发布信用红黑名单制度征求意见稿",
+					},
+					{
+						typeid: 1,
+						id: "1006",
+						date: "2017-05-26",
+						title: "证监会将设立上市公司“光荣榜”和“黑名单”",
+					},
+					{
+						typeid: 1,
+						id: "1007",
+						date: "2016-02-08",
+						title: "2016年社会信用体系建设十大新闻事件",
+					},
+					{
+						typeid: 1,
+						id: "1008",
+						date: "2016-12-13",
+						title: "全国2016年58余万“老赖”主动履行法律义务",
+					}, {
+						typeid: 1,
+						id: "1009",
+						date: "2016-11-30",
+						title: "安徽省社会信用体系建设加快推进",
+					},
+					{
+						typeid: 1,
+						id: "1010",
+						date: "2016-11-01",
+						title: "中国490万“失信者”被限制乘坐飞机",
+					},
+					{
+						typeid: 1,
+						id: "1011",
+						date: "2016-10-24",
+						title: "芜湖市住房公积金个人失信行为将有法可依",
+					},
+					{
+						typeid: 1,
+						id: "1012",
+						date: "2016-10-17",
+						title: "央行四个月内第五次出手 重拳整肃征信市场乱象",
+					},
+					{
+						typeid: 1,
+						id: "1013",
+						date: "2016-10-01",
+						title: "用“纳税信用”即可获无抵押贷款 9万余户企业受益",
+					},
+					{
+						typeid: 1,
+						id: "1014",
+						date: "2016-09-25",
+						title: "中国70万A级纳税人享税收便利",
+					},
+				],
+				newsDetail: {}
+			};
+		},
+		methods: {
+			showDetail: function(current) {
+				console.log(current);
+				this.showList = false;
+				this.newsDetail = this.news[current];
+				this.current = "newsDetail" + (current + 1);
+			},
+			backList() {
+				this.showList = true;
+			}
+		},
+		beforeRouteEnter(to, from, next) {
+			next(vm => {
+				if(to.params.id) {
+					vm.showDetail(to.params.id);
+				}
+			});
+		},
+		mounted: function() {
+			this.$emit("setCrumb", "行业新闻");
+		}
+	};
 </script>
 
 <style lang="less" scoped>
-.company-news {
-  .news-wrapper {
-    li {
-      font-size: 16px;
-      border-bottom: 1px #7d7a74 dashed;
-      color: #7d7a74;
-      padding-bottom: 12px;
-      text-indent: 1.5em;
-      div {
-        margin-top: 12px;
-      }
-    }
-    .title {
-      float: left;
-    }
-    .time {
-      float: right;
-    }
-  }
-}
+	.company-news {
+		.news-wrapper {
+			li {
+				font-size: 16px;
+				border-bottom: 1px #7d7a74 dashed;
+				color: #7d7a74;
+				padding-bottom: 12px;
+				text-indent: 1.5em;
+				div {
+					margin-top: 12px;
+				}
+			}
+			.title {
+				float: left;
+			}
+			.time {
+				float: right;
+			}
+		}
+	}
 </style>
---
+++ src/pages/news/topic/newsDetail6.vue
@@ -0,0 +1,84 @@
+<template>
+	<div class="detail_1">
+		<div class="header">惠国征信助力芜湖创建信用示范城市取得初步成效</div>
+		<div class="nav">
+			<p class="left">发布日期：2017-01-04</p>
+			<p class="right">发布人：芜湖新闻频道&芜湖综合广播</p>
+		</div>
+		<div class="hr1"></div>
+		<div class="p1">2015年7月，芜湖市获批成为全国首批创建社会信用体系建设示范城市之一。一年多来，我市把加强社会信用体系建设作为促进城市治理体系和治理能力现代化的重要内容，以创建社会信用体系建设示范城市为抓手，以打造“诚信芜湖”为目标，全力推进社会信用体系建设，取得了初步成效。</div>
+		<div class="p1">在当前的经济发展环境下，中小企业的转型升级面临着多方面的困难，而由于信息不畅通、不对称，中小企业融资难也成为阻碍企业发展的一大难题。由市政府主导、市信息办、金融办等部门联合推动成立于今年7月份的惠国征信股份有限公司给中小企业融资带来了福音。</div>
+		<div class="p1">惠国征信总经理徐慧囝： 我们并行启动了芜湖市中小企业诚信协会，这个协会本身就是入会企业获得金融扶持的绿色通道，体现了企业信用指纹以及企业信用足迹的全过程，用这个制度和体系来保证企业获得相应的便利的融资和金融的扶持。</div>
+		<div class="p1">通过登记企业信息，了解跟进企业生产发展的过程，形成数据，录入征信系统，企业的信用情况便一目了然。第三方征信机构系统的介入打破了以往企业数据信息闭塞或者不完整的格局，而惠国征信专业的团队更加确保了数据的精准与实效。</div>
+		<div class="p1">惠国征信总经理徐慧囝： 用物联网大数据征信和国内最领先的算法，包括我们58个博士的团队去研发出来的算法，做数学和质量的保证，同时还有来自于政府的数据，本身是一个严密的体系去做数据的收集、整理、清洗和使用，把它们汇集在一起，在数据源头上和数据加工处理上以及产品化的方向上，相对来说能够最大程度保证数据的精准和判断的客观。</div>
+		<div class="p1">近年来，我市着力夯实信用体系建设基础，建设公共信用信息数据库、公共信用信息共享服务平台、“信用芜湖”网站，加快推进统一信用代码进程，着力强化信用信息应用，不断扩大信用影响力。率先向金融领域开放使用信用信息，促进形成叠加互补效应，构建公共资源交易诚信评价体系，推进招投标领域的信用应用。积极引入信用监管，探索创新综合监管方式。</div>
+		<div class="p1">接下来，我市将以社会信用体系建设示范城市验收评估要求为标准，深入推进信用体系建设各项工作，为打造经济和城市两个升级版，助力城市经济社会发展做出积极贡献。</div>
+		<div class="hr2"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['newsDetail'],
+        
+	};
+</script>
+
+<style lang="less" scoped>
+	.detail_1 {
+		.hr1 {
+			margin-top: 40px;
+			margin-bottom: 10px;
+			border: 0;
+			border-top: 1px solid #eee;
+		}
+		.header {
+			font-size: 18px;
+			text-align: center;
+			margin-bottom: 5px;
+			color: #b62a31;
+		}
+		.nav {
+			width: 400px;
+			color: #888;
+			margin: 0 auto;
+			font-size: 12px;
+			.left {
+				float: left;
+				padding-left: 15px;
+			}
+			.right {
+				float: right;
+				padding-right: 15px;
+			}
+		}
+		.img {
+			/*height: 430px;*/
+			width: 700px;
+			margin: 0 auto;
+			text-align: center;
+		}
+		.p1 {
+			font-size: 14px;
+			text-indent: 2em;
+			padding-bottom: 5px;
+			line-height: 1.75;
+			color: #888;
+		}
+		.p2 {
+			font-size: 14px;
+			text-align: center;
+		}
+		.p3{
+			font-size:14px;
+		  	text-align: center;
+		  	color: #888;
+		}
+		.hr2 {
+			margin-top: 20px;
+			margin-bottom: 10px;
+			border: 0;
+			border-top: 1px solid #eee;
+		}
+	}
+</style>
