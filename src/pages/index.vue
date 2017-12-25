7d5d71f0d1521422b9e0e4a509163880a9b9e58d
lizongwen <93700983@qq.com>
2017-12-25 16:51:11 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -28,10 +28,8 @@
 			<div class="news">
 				<ul>
 					<li class="news-item" v-for="(item,index) in news" :key="index" @click="showNews(index)">
-						<div class="title">新闻标题</div>
-						<p class="text-indent detail">
-							国征信服务股份有限公司（以下简称“惠 国征信”）是由芜湖市政府主导推动，芜湖市信 息办、金融办、信用办联合引领的以国征信服务股份有限公司（以下简称“惠 国征信”）是由芜湖市政府主导推动，芜湖市信 息办、金融办、信用办联合引领的以国征信服务股份有限公司（以下简称“惠 国征信”）是由芜湖市政府主导推动，芜湖市信 息办、金融办、信用办联合引领的以
-						</p>
+						<div class="title">{{item.title}}</div>
+						<p class="text-indent detail">{{item.content}}</p>
 					</li>
 				</ul>
 			</div>
@@ -71,7 +69,7 @@ export default {
     return {
       logo_jpg: logo_jpg,
       about: {
-        title: "惠国简介",
+        title: "",
         summary: `惠国征信服务股份有限公司（以下简称“惠国征信”）是由芜湖市政府主导推动，
 				芜湖市信息办、金融办、信用办联合引领的以征信平台为载体、致力于政务信用、
 				中小企业征信以及农业征信等多元化征信产品服务，以金融大数据和商业物联网大数据为基础的综合性高端征信业服务机构。`,
@@ -80,16 +78,23 @@ export default {
       },
       news: [
         {
+          title: "芜湖新闻频道“丹丹面对面”栏目邀请惠国征信徐博士谈征信",
           id: 1,
-          content: ""
+          content: `为更好地了解芜湖市社会信用体系建设现状，2017年9月5日上午，
+		  芜湖新闻频道“丹丹面对面”栏目特邀惠国征信服务股份有限公司董事长兼总裁徐慧囝博士进入演播室，从征信公司的视角，围绕征信建设的宏观背景、
+		  全市信用贷开展情况和惠国征信团队介绍等方面，全面解读芜湖市社会信用体系建设情况。`
         },
         {
+          title: "惠国征信举办《建模方法论SEMMA建模路径》培训",
           id: 2,
-          content: ""
+          content: `为更好的实现信用贷项目的落地和执行，提高团队整体专业技能，拓宽学习和工作思路，2017年8月4日下午，
+		  惠国征信开展了《建模方法论之——SEMMA建模路径》内部专题培训，
+		  由董事长兼总裁徐慧囝博士全程授课，并邀请了芜湖市信用办、市社会信用服务中心、市大数据管理中心相关领导莅临公司指导交流。`
         },
         {
+          title: "惠国征信董事长兼总裁徐慧囝博士应邀参加台州市第三期大数据论坛",
           id: 3,
-          content: ""
+          content: `2017年7月6日晚，台州市“大数据”第3期论坛《信用大数据》会议在台州市应急指挥中心大楼三楼大厅举办，本次活动是台州市应急指挥中心大楼建成首次启用，台州市发改委徐贤军副主任，市经信委信息化处王锦林处长，李林欢副处长，市政府应急办陈炜华主任，市委市政府信息中心陈友增主任参加会议。本次活动由浙江移动台州分公司，台州市信息化促进会共同主办`
         }
       ],
       service: [
@@ -207,6 +212,9 @@ export default {
         }
         .title {
           font-weight: bold;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
         }
         .detail {
           overflow: hidden;
@@ -228,7 +236,9 @@ export default {
           margin-bottom: 0;
         }
         .avatar {
+          display: flex;
           flex: 1;
+          align-items: center;
         }
         .content {
           flex: 1;
